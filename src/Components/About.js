0448cbdda46c2537ee94333906968bb9c59fecd1
wenlianghuang <wenlianghuang@gmail.com>
2021-01-27 23:59:56 +0800

    20210127 modified file

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,7 +18,7 @@ class About extends Component {
       //var resumeDownload = this.props.data.resumedownload;
       var resumeDownload = "images/" + this.props.data.resumeDownload;
       resumeDownload = "https://master.d30eeqjx15uo9v.amplifyapp.com/" + resumeDownload;
-      //var testimage = "https://master.d30eeqjx15uo9v.amplifyapp.com/" + profilepic;
+      var testimage = "https://master.d30eeqjx15uo9v.amplifyapp.com/" + profilepic;
     }
 
     return (
@@ -47,7 +47,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button" download><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={testimage} className="button" download><i className="fa fa-download"></i>Download Resume</a>
                   </p>
                </div>
             </div>
