331b9b6e60da7da507c73eb32569313329dbe590
wenlianghuang <wenlianghuang@gmail.com>
2021-01-28 00:05:54 +0800

    20210127 modified file

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,7 +16,7 @@ class About extends Component {
       var email = this.props.data.email;
       var cell_phone = this.props.data.cell_phone;
       //var resumeDownload = this.props.data.resumedownload;
-      var resumeDownload = "images/" + this.props.data.resumeDownload;
+      var resumeDownload = "images/" + this.props.data.resumedownload;
       resumeDownload = "https://master.d30eeqjx15uo9v.amplifyapp.com/" + resumeDownload;
       console.log('resume: ',resumeDownload);
       var testimage = "https://master.d30eeqjx15uo9v.amplifyapp.com/" + profilepic;
